3186c93c10caedb1afaece2adba27c8fa30b0484
Vitaly Goryaev <lacrom@mail.ru>
2016-10-01 21:40:54 +0300

    default password

--- Tarantool/src/app.lua
+++ Tarantool/src/app.lua
@@ -5,10 +5,10 @@ box.cfg {
 	slab_alloc_arena = 1 -- one gb memory limit
 }
 
-box.schema.user.passwd('admin', os.getenv("ADMIN_PASSWORD"))
+box.schema.user.passwd('admin', os.getenv("ADMIN_PASSWORD") or '123')
 
 box.once('create_user_for_nodejs', function()
-		box.schema.user.create('worker', { password = os.getenv("WORKER_PASSWORD") })
+		box.schema.user.create('worker', { password = os.getenv("WORKER_PASSWORD") or '123' })
 		box.schema.user.grant('worker','read,write,execute','universe')
 end)
 
