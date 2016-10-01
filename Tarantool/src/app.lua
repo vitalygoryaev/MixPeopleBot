86b6462111db2df0994195767825334bbaf2c801
Vitaly Goryaev <lacrom@mail.ru>
2016-10-01 17:52:38 +0300

    added tarantool

---
+++ Tarantool/src/app.lua
@@ -0,0 +1,26 @@
+#!/usr/bin/env tarantool
+
+box.cfg {
+	listen=3301,
+	slab_alloc_arena = 1 -- one gb memory limit
+}
+
+box.schema.user.passwd('admin', os.getenv("ADMIN_PASSWORD"))
+
+box.once('create_user_for_nodejs', function()
+		box.schema.user.create('worker', { password = os.getenv("WORKER_PASSWORD") })
+		box.schema.user.grant('worker','read,write,execute','universe')
+end)
+
+box.once('create_users_space', function()
+		box.schema.space.create('users')
+		box.space.users:create_index('primary', { type = 'tree', unique = true, parts = { 1, 'unsigned' }})
+		box.space.users:create_index('vendor', { type = 'hash', unique = true, parts = { 2, 'str', 3, 'nuunsignedm' }}) -- vendor, vendor's userId
+end)
+
+box.once('create_waitingUsers_space', function()
+		box.schema.space.create('waitingUsers')
+		box.space.waitingUsers:create_index('primary', { type = 'tree', unique = true, parts = { 1, 'unsigned' }})
+end)
+
+require('functions')
---
+++ Tarantool/src/functions.lua
@@ -0,0 +1,101 @@
+json = require('json')
+
+function link(userId) 
+	local user = box.space.users:get({ userId })
+
+	-- check status
+	if user[5] ~= 'idle' then
+		return failJson({ 'user is not idle' })
+	end
+
+	-- set status
+	box.space.users:update(user[1], {{ '=', 5, 'waiting' }})
+	box.space.waitingUsers:insert({ user[1] })
+
+	-- find opponent
+	local opponent = nil
+	
+	while not opponent do
+		-- check if only you is active
+		if box.space.waitingUsers.index.primary:count() <= 1 then
+			return failJson({ 'no one else is waiting' })
+		end
+
+		opponent = box.space.waitingUsers.index.primary:random(os.time())
+
+		if opponent[1] == user[1] then 
+			opponent = nil
+		end
+	end
+
+	-- link together
+	box.space.waitingUsers:delete(user[1])
+	user = box.space.users:update(user[1], {{ '=', 5, 'talking' }, { '=', 6, opponent[1] }})
+
+	box.space.waitingUsers:delete(opponent[1])
+	opponent = box.space.users:update(opponent[1], {{ '=', 5, 'talking' }, { '=', 6, user[1] }})
+
+	return successJson({ user = getUserObject(user), opponent = getUserObject(opponent) })
+end
+
+function unlinkAndStop(userId)
+	local user = box.space.users:get({ userId })
+
+	if user[5] == 'talking' then
+		user = unlink(userId)
+	end
+
+	user = box.space.users:update(user[1], {{ '=', 5, 'idle' }})
+
+	return successJson(getUserObject(user))
+end
+
+function next(userId)
+	local user = box.space.users:get({ userId })
+
+	-- check status
+	if user[5] ~= 'talking' then
+		return failJson({ 'user is not talking' })
+	end
+
+	unlink(userId)
+	return link(userId)
+end
+
+function getUser(vendor, vendorUserId, name)
+	local user = box.space.users.index.vendor:get({ vendor, vendorUserId })
+
+	if not user then
+		user = box.space.users:auto_increment({ vendor, vendorUserId, name, 'idle' })
+	end
+
+	return successJson(getUserObject(user))
+end
+
+function unlink(userId)
+	local user = box.space.users:get({ userId })
+
+	if user[5] ~= 'talking' then 
+		return failJson({ 'user is not talking' })
+	end
+
+	if user[6] then
+		local opponent = box.space.users:get({ user[6] })
+		opponent = box.space.users:update(opponent[1], {{ '=', 5, 'waiting' }, { '#', 6, nil }})
+	end
+
+	user = box.space.users:update(user[1], {{ '=', 5, 'waiting' }, { '#', 6, nil }})
+	return user
+end 
+
+function successJson(result)
+	return json.encode({ success = true, result = result })
+end
+
+function failJson(errorList)
+	return json.encode({ success = false, errorList = errorList })
+end
+
+function getUserObject(user)
+	return { id = user[1], vendor = user[2], vendorUserId = user[3], name = user[4], status = user[5], opponent = user[6]}
+end
