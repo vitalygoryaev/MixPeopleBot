84ab4f0a39492d54cb8a3647dd2a91ab7b314811
goryaev <goryaev@alor.ru>
2016-10-05 15:56:06 +0300

    trying to fix queue

--- worker/index.js
+++ worker/index.js
@@ -1,7 +1,11 @@
 "use strict";
-let queue = require('./lib/queue.js').get('rabbit', 'worker', 'workwork', 'messages');
+let queue;
+require('./lib/queue.js').get('rabbit', 'worker', 'workwork', 'messages')
+    .then(queue => {
+        queue = queue;
 
-queue.subscribe(function handler(content) {
-    console.log(content.vendor);
-    console.log('got message from queue: ', content.message);
-})
+        queue.subscribe(function handler(content) {
+            console.log(content.vendor);
+            console.log('got message from queue: ', content.message);
+        })
+    })
