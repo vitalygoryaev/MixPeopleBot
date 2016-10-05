288557751d1957d6e439880b576daf8c6d537929
goryaev <goryaev@alor.ru>
2016-10-05 17:46:18 +0300

    added tarantool call to worker

--- worker/index.js
+++ worker/index.js
@@ -4,8 +4,16 @@ require('./lib/queue.js').get('rabbit', 'worker', 'workwork', 'messages')
     .then(queue => {
         queue = queue;
 
+        return require('./lib/tarantool.js').get('worker', 'workwork');
+    })
+    .then(tarantool => {
         queue.subscribe(function handler(content) {
             console.log(content.vendor);
             console.log('got message from queue: ', content.message);
+
+            tarantool.getUser(content.vendor, content.message.chat.id, content.message.chat.first_name)
+                .then(result => {
+                    console.log('got result from getUser tarantool\n', result);
+                })
         })
     })
