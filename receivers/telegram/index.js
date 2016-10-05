02b70af3a07dd380a11de7b2d72692845300cde8
goryaev <goryaev@alor.ru>
2016-10-05 16:05:13 +0300

    telegram receiver using promises

--- receivers/telegram/index.js
+++ receivers/telegram/index.js
@@ -1,24 +1,34 @@
 let telegram = require('telegram-bot-api');
-let queue = require('../lib/queue.js').get('rabbit', 'worker', 'workwork', 'messages');
 
-let api = new telegram({
+let queue;
+require('./lib/queue.js').get('rabbit', 'worker', 'workwork', 'messages')
+    .then(queue => {
+        queue = queue;
+		return queue;
+    })
+	.then(initTelegram);
+
+function initTelegram(queue) {
+	let api = new telegram({
         token: '226303585:AAESI73YnfVa3v8gxVAhXCmc0eEvG7tUePY',
 		updates: {
             enabled: true,
 			get_interval: 100
     	}
-});
-
-api.getMe()
-	.then(function(data)
-	{
-		console.log(data);
-	})
-	.catch(function(err)
-	{
-		console.log(err);
 	});
 
-api.on('message', function(message) {
-	queue.push('telegram', message);
-});
+	api.getMe()
+		.then(function(data)
+		{
+			console.log(data);
+		})
+		.catch(function(err)
+		{
+			console.log(err);
+		});
+
+	api.on('message', function(message) {
+		queue.push('telegram', message);
+	});
+}
+
